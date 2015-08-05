060f2e123597277de05ddb3825591ff60fe04f2b
Hapax <MJSilk2@gmail.com>
2015-08-05 20:54:30 +0100

    Complete Version (1.0.0)
    Updated class to complete version (1.0.0).
    Updated readme to match.
    Example now has more features and controls.
    New resources have been added while old resources have been renamed
    slightly.

--- readme.md
+++ readme.md
@@ -1,29 +1,46 @@
 #Sprite3d
-*by Hapax (2015)*
+*by Hapax (August 2015)*
 (http://github.com/Hapaxia)
 
 For use with [SFML](http://sfml-dev.org) (v2).
 
-Originally planned to be a part of the drawables module in the [Hx](http://github.com/hapaxia/Hx) library, this follow-up to [SpinningCard](https://github.com/Hapaxia/SpinningCard), which has been completely rewritten from scratch, has been re-routed to a life of independence!
+Originally planned to be a part of a drawables module in the [Hx](http://github.com/Hapaxia/Hx) library, this follow-up to [SpinningCard](https://github.com/Hapaxia/SpinningCard), which has been completely rewritten from scratch, has been re-routed to a life of independence!
 
 This is a drawable class for use with SFML. It works in a similar way to a SFML standard Sprite (it has many of the same methods plus it's derived from both sf::Transformable and sf::Drawable). This class, however, has the ability to rotate around all three axis (the standard Sprite only rotates around the z axis).
 
 Since it can rotate and show its back face, the class allows you to supply a secondary texture which it displays instead when the back is visible.
 
-Click this image to see a video that shows what the provided example produces:
+Click this image to see a video that shows an early test example:
 [![Example Video](http://img.youtube.com/vi/EAPDIlVEMKA/0.jpg)](http://www.youtube.com/watch?v=EAPDIlVEMKA)
 
 Note that the texture is still subject to the same mapping problems evident in [SpinningCard](https://github.com/Hapaxia/SpinningCard) where it shows that distorting a quad doesn't interpolate the texture correctly.
-As in [SpinningCard](https://github.com/Hapaxia/SpinningCard), this object is, instead of being created using a quad (or two triangles), created using a triangle fan that uses either four or eight (can be chosen) triangles to display to quad. This helps keep certain areas (centre point and centre of edges or just centre point) of the texture in the correct place but still does not distort the texture perfectly.
+As in [SpinningCard](https://github.com/Hapaxia/SpinningCard), this object can, instead of being created using a quad (or two triangles), be created using more triangles to reduce the size - and therefore the distortions in the textures - of the triangles. Sprite3d creates a triangle strip that alternates across each row of quads. The number of triangles is determined by the Mesh Density and the Subdivision level (see below).
 
-The texture distortions are more visible depending on the apparent depth of the 3D effect. This amount can be changed. It's called shallowness: the higher the value, the less depth but with less visible texture distortion, whereas the lower the value, the greater the depth but with more visible texture distortion.
+The texture distortions are more visible depending on the apparent depth of the 3D effect. This amount can be changed by setting its **depth**, which is a _float_. The higher the value, the greater the apparent depth but with more visible texture distortion; the lower the value, the flatter the apparent depth but with less visible texture distortion. The default depth value is 10.
 
-This class is a work-in-progress and as such should not be considered complete or final. Some features of the standard sprite are currently missing such as setting custom texture rectangles and getting bounds.
+To combat the texture distortion evident when a non-affine transformation (three dimensional rotation is a non-affine transformation as the edges stop being parallel) is applied to a quad, there are three features in Sprite3d that alter the mesh (size and number of triangles).
 
-Planned for the future is dynamic splitting of the sprite into multiple quads (triangle pair) to allow how accurate the texture will look by using more vertices.
+1. *Mesh Density*
+This is how many points are used per dimension, not including the edges.
+For example, if the mesh is created from 5x5 points, that's 5 points per dimension - 3 points per dimension, not including the edges.
+So, a mesh density of 3 would give you a mesh created from 5x5 points (a single quad is 2x2 points; 5x5 points creates 4x4 quads)
 
-Due to this currently being unfinished, some of the interface should be expected to change ([that bool](Sprite3d.hpp#L52) will probably disappear, for example). However, the main interface should stay relatively stable since it's designed to be used in the same way that SFML's standard Sprite is used.
+2. *Subdivision level*
+This is very similar to Mesh Density. It changes how many points are used for the mesh.
+Each subdivision divides each quad in the mesh into four quads.
+e.g.:
+1 quad that is subdivided once becomes 4 quads,
+1 quad that is subdivided twice becomes 16 quads,
+etc.
 
-The class will be posted to the [SFML Forum's project forum](http://en.sfml-dev.org/forums/index.php?board=10.0) or the [SFML Forum's wiki forum](http://en.sfml-dev.org/forums/index.php?board=11.0) at a later time.
+3. *Dynamic Subdivision*
+When enabled, Dynamic Subdivision changes the subdivision level automatically based on the most extreme angle. That is, the greatest rotation angle of pitch and yaw (around the x and y axes respectively). The range of subdivision can be specified; the default range is 1 to 4.
+Example:
+Using the default range, if pitch and yaw are both 0 (object is _flat_), the subdivision level would be 1, whereas if pitch _or_ yaw is close to 90°, the subdivision level would be 3. Subdivision level 2 would be used if the most extreme angle was not almost 90° but was not almost flat.
+The range is interpolated linearly so (in the case of the default range) from 0° _up to_ 30° the subdivision level would be 1, from 30° _up to_ 60° degrees it would be 2, from 60° _up to_ 90° it would be 3, and _at_ 90° it would be 4.
+
+Note: if you used Sprite3d in its previous form, the interface may be slightly different. The main interface should be compatible and most of the changes to the interface are simply additional methods.
+
+There is a [thread dedicated to this class](http://en.sfml-dev.org/forums/index.php?topic=18698) on the SFML wiki forum.
 
 [Hapax's SFML Forum profile](http://en.sfml-dev.org/forums/index.php?action=profile;u=13086)
--- sprite3dExample.cpp
+++ sprite3dExample.cpp
@@ -1,5 +1,33 @@
+//////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Sprite3d EXAMPLE
+//
+//  by Hapax (http://github.com/Hapaxia)
+//
+//
+//    Key controls:
+//
+//  Escape          Quit
+//  Space           Pause/unpause
+//  Backspace       Reset clock
+//  - =             Adjust depth
+//  [ ]             Adjust mesh density
+//  , .             Adjust subdivision level
+//  Tab             Toggle textures
+//  Return          Toggle Dynamic Subdivision (default range is 1 to 3)
+//  B               Toggle back face correction (flip)
+//  F8              Resets mesh to minimal (two triangles) and turns off Dynamic Subdivision
+//  F9              Toggle visibility of Sprite3d's bounds
+//  F10             Toggle visibility of standard sprite's bounds
+//  F11             Toggle visibility of Sprite3d
+//  F12             Toggle visibility of standard sprite
+//
+//  Please note that this example makes use of C++11 features
+//
+//////////////////////////////////////////////////////////////////////////////////////////////
+
 #include <SFML/Graphics.hpp>
-#include <iostream>
+#include <iostream> // for std::cerr
 
 #include "Sprite3d.hpp"
 
@@ -7,12 +35,18 @@ int main()
 {
 	// set up window
 	sf::RenderWindow window(sf::VideoMode(800, 600), "Sprite3d", sf::Style::Default);
-	window.setVerticalSyncEnabled(true);
+	//window.setVerticalSyncEnabled(true);
 
 	// load resources (textures and font)
-	sf::Texture texture, backTexture;
-	if (!texture.loadFromFile("resources/uv map 1.jpg") ||
-		!backTexture.loadFromFile("resources/uv map 1 (edit).jpg"))
+	sf::Texture texture, frontTexture, backTexture;
+	if (!texture.loadFromFile("resources/uv map.jpg") ||
+		!frontTexture.loadFromFile("resources/uv map front.jpg") ||
+		!backTexture.loadFromFile("resources/uv map back.jpg"))
+	/*
+	if (!texture.loadFromFile("resources/Card Face - SFML.png") ||
+		!frontTexture.loadFromFile("resources/Card Face - SFML.png") ||
+		!backTexture.loadFromFile("resources/Card Back - SFML.png"))
+	*/
 	{
 		std::cerr << "Error loading textures." << std::endl;
 		return EXIT_FAILURE; // textures are required for this example. it's pointless carrying on without them
@@ -29,25 +63,44 @@ int main()
 	sprite3dText.setPosition(sf::Vector2f(window.getSize().x * 0.25f, 0.f));
 	spriteText.setPosition(sf::Vector2f(window.getSize().x * 0.75f, 0.f));
 
-	// text depth value
-	sf::Text depthText("Shallowness: ", font, 24);
-	depthText.setOrigin(depthText.getLocalBounds().left, depthText.getLocalBounds().top + depthText.getLocalBounds().height);
-	depthText.setPosition(sf::Vector2f(0.f, window.getSize().y * 1.f));
+	// text feedback
+	sf::Text feedbackText("FPS:\nVertices:\nSubdivision Level:\nSubdivided Mesh Density:\nMesh Density:\nDynamic Subdivision enabled:\nMost Extreme Angle:\nDepth:", font, 16);
+	feedbackText.setOrigin(feedbackText.getLocalBounds().left, feedbackText.getLocalBounds().top + feedbackText.getLocalBounds().height);
+	feedbackText.setPosition(sf::Vector2f(2.f, window.getSize().y - 1.f));
 
 	// sprite3d
-	sf::Sprite3d sprite3d(texture, backTexture);
-	sprite3d.setOrigin(sf::Vector2f(sprite3d.getTexture()->getSize()) / 2.f);
+	sf::Sprite3d sprite3d(frontTexture, backTexture);
+	sprite3d.setOrigin(sf::Vector2f(sprite3d.getLocalBounds().width, sprite3d.getLocalBounds().height) / 2.f);
 	sprite3d.setPosition(sf::Vector2f(window.getSize().x * 0.25f, window.getSize().y / 2.f));
+	//sprite3d.setBackFlipEnabled();
+	//sprite3d.setMeshDensity(3); // = 5x5 points = 4x4 quads
+	//sprite3d.setNumberOfPoints(25); // 5x5
+	//sprite3d.setNumberOfQuads(16); // 4x4
+	//sprite3d.setDynamicSubdivisionRange(3, 0); // max: 32x32 quads (4 subdivided 3 times is 32), min: 4x4 quads (zero subdivisions)
 
 	// sprite
+	//sf::Sprite sprite(sprite3d.getSprite());
 	sf::Sprite sprite(texture);
-	sprite.setOrigin(sf::Vector2f(sprite3d.getTexture()->getSize()) / 2.f);
+	sprite.setOrigin(sf::Vector2f(sprite.getLocalBounds().width, sprite.getLocalBounds().height) / 2.f);
 	sprite.setPosition(sf::Vector2f(window.getSize().x * 0.75f, window.getSize().y / 2.f));
+
+	// bounds rectangles
+	sf::RectangleShape boundsSprite3d;
+	boundsSprite3d.setFillColor(sf::Color::Transparent);
+	boundsSprite3d.setOutlineColor(sf::Color::Red);
+	boundsSprite3d.setOutlineThickness(1.f);
+	sf::RectangleShape boundsSprite = boundsSprite3d;
 
 	// clock and pause
 	sf::Clock clock;
-	bool isPaused{ false };
+	bool isPaused{ true };
 	float time{ 0.f };
+
+	// visibility flags
+	struct
+	{
+		bool     sprite3d, sprite, sprite3dBounds, spriteBounds;
+	} isVisible{ true,     true,   false,          false };
 
 	while (window.isOpen())
 	{
@@ -65,20 +118,92 @@ int main()
 					clock.restart();
 				}
 
+				// reset time
+				else if (event.key.code == sf::Keyboard::BackSpace)
+				{
+					time = 0.f;
+					clock.restart();
+				}
+
 				// control depth
 				else if (event.key.code == sf::Keyboard::Equal)
 					sprite3d.setDepth(sprite3d.getDepth() + 1.f);
 				else if (event.key.code == sf::Keyboard::Dash)
 					sprite3d.setDepth(sprite3d.getDepth() - 1.f);
+
+				// control mesh density
+				else if (event.key.code == sf::Keyboard::RBracket)
+					sprite3d.setMeshDensity(sprite3d.getMeshDensity() + 1);
+				else if (event.key.code == sf::Keyboard::LBracket)
+				{
+					if (sprite3d.getMeshDensity() > 0)
+						sprite3d.setMeshDensity(sprite3d.getMeshDensity() - 1);
+				}
+
+				// control subdivision level
+				else if (event.key.code == sf::Keyboard::Period)
+					sprite3d.setSubdivision(sprite3d.getSubdivision() + 1);
+				else if (event.key.code == sf::Keyboard::Comma)
+				{
+					if (sprite3d.getSubdivision() > 0)
+						sprite3d.setSubdivision(sprite3d.getSubdivision() - 1);
+				}
+
+				// toggle dynamic subdivision
+				else if (event.key.code == sf::Keyboard::Return)
+					sprite3d.setDynamicSubdivisionEnabled(!sprite3d.getDynamicSubdivisionEnabled());
+
+				// toggle back face is flipped state
+				else if (event.key.code == sf::Keyboard::B)
+					sprite3d.setBackFlipEnabled(!sprite3d.getBackFlipEnabled());
+
+				// toggle bounds rectangles
+				else if (event.key.code == sf::Keyboard::F9)
+					isVisible.sprite3dBounds = !isVisible.sprite3dBounds;
+				else if (event.key.code == sf::Keyboard::F10)
+					isVisible.spriteBounds = !isVisible.spriteBounds;
+
+				// toggle whether or not to draw the sprites
+				else if (event.key.code == sf::Keyboard::F11)
+					isVisible.sprite3d = !isVisible.sprite3d;
+				else if (event.key.code == sf::Keyboard::F12)
+					isVisible.sprite = !isVisible.sprite;
+
+				// reset the mesh to minimal (also turns off dynamic subdivision)
+				else if (event.key.code == sf::Keyboard::F8)
+				{
+					sprite3d.setDynamicSubdivisionEnabled(false);
+					sprite3d.minimalMesh();
+				}
+
+				// toggle texture usage
+				else if (event.key.code == sf::Keyboard::Tab)
+				{
+					if (sprite3d.getTexture() != &frontTexture)
+					{
+						sprite3d.setTexture(frontTexture);
+						sprite3d.setBackTexture(backTexture);
+					}
+					else
+					{
+						sprite3d.setTexture(sf::Texture());
+						sprite3d.setBackTexture(sf::Texture());
+					}
+				}
 			}
 		}
 
 		// update time
+		float frameTime = 1.f;
 		if (!isPaused)
-			time += clock.restart().asSeconds();
+		{
+			frameTime = clock.restart().asSeconds();
+			time += frameTime;
+		}
 
 		// update sprites
 		sprite3d.setRotation({ time * 97.f, time * 42.f, time * 51.f });
+
 		//sprite.setScale({ cos(time * 97.f * 3.141592653f / 180.f), cos(time * 42.f * 3.141592653f / 180.f) });
 		sprite.setScale({ cos(time * 42 * 3.141592653f / 180.f), cos(time * 97 * 3.141592653f / 180.f) });
 		sprite.setRotation(time * 51.f);
@@ -90,16 +215,40 @@ int main()
 		*/
 		//sprite3d.setYaw(time * 180.f);
 
+		// update bounds rectangle
+		boundsSprite3d.setPosition(sf::Vector2f(sprite3d.getGlobalBounds().left, sprite3d.getGlobalBounds().top));
+		boundsSprite3d.setSize(sf::Vector2f(sprite3d.getGlobalBounds().width, sprite3d.getGlobalBounds().height));
+		boundsSprite.setPosition(sf::Vector2f(sprite.getGlobalBounds().left, sprite.getGlobalBounds().top));
+		boundsSprite.setSize(sf::Vector2f(sprite.getGlobalBounds().width, sprite.getGlobalBounds().height));
+
 		// update depth text
-		depthText.setString("Depth: " + std::to_string(sprite3d.getDepth()));
+		unsigned int subdividedMeshDensity = sprite3d.getMeshDensity();
+		for (unsigned int i = 0; i < sprite3d.getSubdivision(); ++i)
+			subdividedMeshDensity = subdividedMeshDensity * 2 + 1;
+		unsigned int numberOfVerticesInSubdividedMesh = (subdividedMeshDensity * 2 + 5) * subdividedMeshDensity + 4;
+		feedbackText.setString(
+			"FPS: " + std::to_string(static_cast<unsigned int>(1.f / frameTime)) +
+			"\nVertices: " + std::to_string(numberOfVerticesInSubdividedMesh) + " (" + std::to_string(numberOfVerticesInSubdividedMesh - 2) + " triangles)" +
+			"\nSubdivision Level: " + std::to_string(sprite3d.getSubdivision()) +
+			"\nSubdivided Mesh Density: " + std::to_string(sprite3d.getSubdividedMeshDensity()) + " (" + std::to_string(sprite3d.getSubdividedMeshDensity() + 2) + "x" + std::to_string(sprite3d.getSubdividedMeshDensity() + 2) + " = " + std::to_string((sprite3d.getSubdividedMeshDensity() + 2) * (sprite3d.getSubdividedMeshDensity() + 2)) + " points)" +
+			"\nMesh Density: " + std::to_string(sprite3d.getMeshDensity()) + " (" + std::to_string(sprite3d.getMeshDensity() + 2) + "x" + std::to_string(sprite3d.getMeshDensity() + 2) + " = " + std::to_string((sprite3d.getMeshDensity() + 2) * (sprite3d.getMeshDensity() + 2)) + " points)" +
+			"\nDynamic Subdivision enabled: " + (sprite3d.getDynamicSubdivisionEnabled() ? "true" : "false") +
+			"\nMost Extreme Angle: " + std::to_string(sprite3d.getMostExtremeAngle()) +
+			"\nDepth: " + std::to_string(sprite3d.getDepth()));
 
 		// update display
-		window.clear();
-		window.draw(sprite);
-		window.draw(sprite3d);
+		window.clear(sf::Color(64, 64, 64));
+		if (isVisible.sprite)
+			window.draw(sprite);
+		if (isVisible.sprite3d)
+			window.draw(sprite3d);
+		if (isVisible.spriteBounds)
+			window.draw(boundsSprite);
+		if (isVisible.sprite3dBounds)
+			window.draw(boundsSprite3d);
 		window.draw(spriteText);
 		window.draw(sprite3dText);
-		window.draw(depthText);
+		window.draw(feedbackText);
 		window.display();
 	}
 
